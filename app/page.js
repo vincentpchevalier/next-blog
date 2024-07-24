81a900636158d3b1b31c085a148706d7b1a463f5
vincentpchevalier <vincentpchevalier@proton.me>
2024-07-24 12:44:37 -0400

    add content to Home page

--- app/page.js
+++ app/page.js
@@ -1,9 +1,12 @@
+import Link from 'next/link';
 import styles from './page.module.css';
 
 export default function Home() {
 	return (
 		<main className={styles.main}>
-			<h1>My Blog</h1>
+			<h1>My Cool Blog</h1>
+			<p>A blog for all my thoughts and ideas</p>
+			<Link href="/blog">See Posts</Link>
 		</main>
 	);
 }
